ac490e2bf0b6eb13eeae7ed5b7416f8ec73226bf
Jardim <gabrielrlj99@outlook.com>
2020-05-24 21:43:26 -0300

    api crud de clientes

---
+++ src/main/java/com/crud/api/documents/Cliente.java
@@ -0,0 +1,59 @@
+package com.crud.api.documents;
+
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Cliente {
+
+	@Id
+	private String id;
+	private String nome;
+	private String email;
+	private String cpf;
+
+	public Cliente() {
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@NotBlank(message = "Nome não pode ser vazio")
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@NotBlank(message = "Email não pode ser vazio")
+	@Email(message = "Email inválido")
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@NotBlank(message = "CPF não pode ser vazio")
+	@CPF(message = "CPF inválido")
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+}
